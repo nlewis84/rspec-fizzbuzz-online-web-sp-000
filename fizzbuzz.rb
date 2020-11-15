9306155bbdb36dafddb72bf32af31a7729634953
Nathan Lewis <n.lewis.perc@gmail.com>
2020-11-15 22:18:23 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,12 +2,12 @@
 # See README.md for instructions on how to do this
 
 def fizzbuzz(num)
-  if num % 3 && num % 5 == 0
-    puts "FizzBuzz"
-  elsif num % 5 == 0
-    puts "Buzz"
-  elsif num % 3 == 0
-    puts "Fizz"
+  if num % 15 === 0
+    "FizzBuzz"
+  elsif num % 5 === 0
+    "Buzz"
+  elsif num % 3 === 0
+    "Fizz"
   else nil
   end
 end
